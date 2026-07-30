e3b597479016842ac5529cf9baaf7136e16b7892
coder8762-debug <coder8762@gmail.com>
2026-07-30 20:16:58 +0500

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,1 +1,36 @@
+import { supabase } from "./supabase.js";
 
+
+const adminEmail = document.getElementById("adminEmail");
+const logoutBtn = document.getElementById("logoutBtn");
+
+
+// Check Login
+
+const { data } = await supabase.auth.getSession();
+
+
+if(!data.session){
+
+    window.location.href = "login.html";
+
+}
+else{
+
+    adminEmail.innerText = data.session.user.email;
+
+}
+
+
+
+// Logout
+
+logoutBtn.addEventListener("click", async()=>{
+
+
+    await supabase.auth.signOut();
+
+    window.location.href="login.html";
+
+
+});
