b7dd8beddf1c5806b890f20cc8d894bd2b4dc4b1
coder8762-debug <coder8762@gmail.com>
2026-07-30 20:20:22 +0500

    Update add-blog.js

--- add-blog.js
+++ add-blog.js
@@ -1,1 +1,43 @@
+import { supabase } from "./supabase.js";
 
+
+const saveBtn = document.getElementById("saveBtn");
+
+
+saveBtn.addEventListener("click", async()=>{
+
+
+const title = document.getElementById("title").value;
+const slug = document.getElementById("slug").value;
+const content = document.getElementById("content").value;
+const image = document.getElementById("image").value;
+
+
+
+const {error} = await supabase
+.from("blogs")
+.insert([
+{
+title:title,
+slug:slug,
+content:content,
+image_url:image
+}
+]);
+
+
+if(error){
+
+alert(error.message);
+
+}
+else{
+
+alert("Blog Saved Successfully!");
+
+window.location.href="dashboard.html";
+
+}
+
+
+});
