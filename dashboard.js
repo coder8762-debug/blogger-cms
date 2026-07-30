4dd56b268029cb7e4146e6918411e0771d4b8bc0
coder8762-debug <coder8762@gmail.com>
2026-07-30 20:24:13 +0500

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,11 +1,15 @@
 import { supabase } from "./supabase.js";
 
 
+// Elements
+
 const adminEmail = document.getElementById("adminEmail");
 const logoutBtn = document.getElementById("logoutBtn");
+const totalBlogs = document.getElementById("totalBlogs");
 
 
-// Check Login
+
+// Check User Login
 
 const { data } = await supabase.auth.getSession();
 
@@ -20,6 +24,38 @@ else{
     adminEmail.innerText = data.session.user.email;
 
 }
+
+
+
+// Get Total Blogs Count
+
+async function loadBlogCount(){
+
+
+    const { count, error } = await supabase
+    .from("blogs")
+    .select("*", { count:"exact", head:true });
+
+
+
+    if(error){
+
+        console.log(error.message);
+
+    }
+    else{
+
+        totalBlogs.innerText = count;
+
+    }
+
+
+}
+
+
+
+loadBlogCount();
+
 
 
 
